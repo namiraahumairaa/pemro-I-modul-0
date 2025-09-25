62df604a2424aa44091fb21e0767c7cd09cbed11
namiraahumairaa <namiraahumairaa@gmail.com>
2025-09-26 06:36:28 +0800

    PRAK002-2510817220008-NamiraHumaira.c

--- PRAK002-2510817220008-NamiraHumaira.c
+++ PRAK002-2510817220008-NamiraHumaira.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main () {
-    printf("Selamat Pagi, Namira Humaira \n\n Selamat Siang, Namira Humairan \n\n Selamat Malam, Namira Humaira");
+    printf("Selamat Pagi, Namira Humaira \n\n Selamat Siang, Namira Humaira \n\n Selamat Malam, Namira Humaira");
     return 0;
 
 }
