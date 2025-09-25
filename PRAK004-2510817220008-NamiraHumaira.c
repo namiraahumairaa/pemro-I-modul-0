d18d65171e797105f78466d14beca2e70a58752a
namiraahumairaa <namiraahumairaa@gmail.com>
2025-09-26 06:39:27 +0800

    PRAK004-2510817220008-NamiraHumaira.c

--- PRAK004-2510817220008-NamiraHumaira.c
+++ PRAK004-2510817220008-NamiraHumaira.c
@@ -4,7 +4,7 @@ int main() {
     printf("##############################\n");
     printf("#                            #\n");
     printf("#        Namira Humaira      #\n");
-    printf("#       2510817220008        #\n");
+    printf("#        2510817220008       #\n");
     printf("#                            #\n");
     printf("##############################\n");
     return 0;
